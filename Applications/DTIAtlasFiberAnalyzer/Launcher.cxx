a0ef985f07b90f1bd6f4b0f29694cccb1e4a666c
Francois Budin <fbudin@unc.edu>
2013-09-11 18:29:55 +0000

    BUG: Launcher.cxx had not been added

---
+++ Applications/DTIAtlasFiberAnalyzer/Launcher.cxx
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "DTIAtlasFiberAnalyzerCLP.h"
+#include <string>
+
+int main( int argc , char* argv[] )
+{
+  PARSE_ARGS;
+  std::string path = argv[0] ;
+  unsigned int found = path.find_last_of( "/" ) ;
+  path = path.substr( 0 , found ) ;
+  #ifdef WIN32
+  std::string cmd = path + "/../hidden-cli-modules/DTIAtlasFiberAnalyzer.exe" ;
+  #else
+  std::string cmd = path + "/../hidden-cli-modules/DTIAtlasFiberAnalyzer" ;
+  #endif
+  for( int i = 1 ; i < argc ; i++ )
+  {
+	  cmd += " " ;
+	  cmd += argv[ i ] ;
+  }
+  std::cout<< cmd << std::endl ;
+  int val = system( cmd.c_str() ) ;
+  return val ;
+}
