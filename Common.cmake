e67264496b471b49cc6d87679d3e99af41488a21
Francois Budin <fbudin@unc.edu>
2014-07-30 15:26:33 +0000

    ENH: Remove unnecessary CMake code

--- Common.cmake
+++ Common.cmake
@@ -73,11 +73,7 @@ include(SlicerExtensionsConfigureMacros)
 #-----------------------------------------------------------------------------
 # CMake Function(s) and Macro(s)
 #-----------------------------------------------------------------------------
-if(CMAKE_PATCH_VERSION LESS 3)
-  include(Pre283CMakeParseArguments)
-else()
-  include(CMakeParseArguments)
-endif()
+include(CMakeParseArguments)
 
 
 
