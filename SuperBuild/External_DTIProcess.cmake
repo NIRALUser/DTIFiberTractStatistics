4a67b4b56378b95e4801316311532c44c04f2e92
Juan Carlos Prieto <juanprietob@gmail.com>
2016-05-12 10:23:51 -0400

    STYLE: Update external project versions
    Update superbuild for qt5, latest qwt version and update DTIProcessToolkit

--- SuperBuild/External_DTIProcess.cmake
+++ SuperBuild/External_DTIProcess.cmake
@@ -76,7 +76,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
 
   ### --- End Project specific additions
   set( ${proj}_REPOSITORY ${git_protocol}://github.com/NIRALUser/DTIProcessToolkit.git)
-  set( ${proj}_GIT_TAG 7f4a3ba68dc9533e4e4aab481c4b36a9c6400fa9 )
+  set( ${proj}_GIT_TAG 89b4dbd03575995da5b0df7585c2d51ad7329245 )
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
